8ede6690cbb1149b5a15fbc62cff56b91bac4b1e
badshasha <shavendragoesoft@gmail.com>
2022-05-07 09:05:44 +0530

    simple dependency injection

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Microsoft.Extensions.DependencyInjection;
+
+namespace dependencyInjection
+{
+    class program {
+
+        public static void Main(String[] args) {
+
+            var serviceCollection = new ServiceCollection();
+            serviceCollection.AddTransient<SimpleClass>();
+
+            var provider = serviceCollection.BuildServiceProvider();
+            var obj1 = provider.GetService<SimpleClass>();
+            var obj2 = provider.GetService<SimpleClass>();
+
+            Console.WriteLine($"{obj1.RandomNumber()}");
+            Console.WriteLine($"{obj2.RandomNumber()}");
+        }
+    }
+}
---
+++ SimpleClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace dependencyInjection
+{
+    public class SimpleClass
+    {
+        public int RandomNumber() {
+            return new Random().Next(1, 10);
+        }
+    }
+}
